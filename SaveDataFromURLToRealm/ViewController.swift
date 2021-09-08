45d0a2871b6923dac6649f06781fa4a471b75b3c
TDB-star <tatiana.blache@gmail.com>
2021-09-08 16:37:12 +0200

    Initial Commit

---
+++ SaveDataFromURLToRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveDataFromURLToRealm
+//
+//  Created by Tatiana Dmitrieva on 08/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
